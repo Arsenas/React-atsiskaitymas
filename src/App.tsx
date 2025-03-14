fea14fb5f2a57777e90f5ff9369f1aa073f4ae1c
Arsenas <business.aval@gmail.com>
2025-03-14 11:51:39 +0200

    Prideti pagrindiniai stiliai ir klases komponentams

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,26 @@ import RecipePage from "./pages/RecipePage";
 import FavoritesPage from "./pages/FavoritesPage";
 import ReviewsPage from "./pages/ReviewsPage";
 import AddRecipePage from "./pages/AddRecipePage";
-import Navbar from "./components/Navbar";
 import EditRecipePage from "./pages/EditRecipePage";
+import Navbar from "./components/Navbar";
+import "./styles/main.scss"; // Importuojam stilius
 
 const App = () => {
   return (
-    <>
+    <div className="app-container"> {/* Pagrindinis puslapio konteineris */}
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipePage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/reviews" element={<ReviewsPage />} />
-        <Route path="/add-recipe" element={<AddRecipePage />} />
-        <Route path="/edit-recipe/:id" element={<EditRecipePage />} />
-      </Routes>
-    </>
+      <div className="content"> {/* Vieta, kur atvaizduojami puslapiai */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/reviews" element={<ReviewsPage />} />
+          <Route path="/add-recipe" element={<AddRecipePage />} />
+          <Route path="/edit-recipe/:id" element={<EditRecipePage />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
 export default App;
-
-//komentaras testinimui, veliau istrinsiu
-//komentaras testinimui, veliau istrinsiu
-//komentaras testinimui, veliau istrinsiu
-//komentaras testinimui, veliau istrinsiu
-//komentaras testinimui, veliau istrinsiu
-//komentaras testinimui, veliau istrinsiu
