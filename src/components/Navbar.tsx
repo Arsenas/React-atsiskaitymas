6576090d3c0d970a97a91ea4754d34fe559d031e
Arsenas <business.aval@gmail.com>
2025-03-14 21:23:26 +0200

    pilnai pakeistas atsiliepimu funkcionalumas

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,10 +7,10 @@ const Navbar = () => {
         <h1 className="logo">Recipe Finder</h1>
         <ul className="nav-list">
           <li><Link to="/">Pagrindinis</Link></li>
+          <li><Link to="/add-recipe">Pridėti receptą</Link></li>
           <li><Link to="/all-recipes">Visi receptai</Link></li>
           <li><Link to="/favorites">Megstamiausi</Link></li>
           <li><Link to="/reviews">Atsiliepimai</Link></li>
-          <li><Link to="/add-recipe">Pridėti receptą</Link></li>
         </ul>
       </div>
     </nav>
